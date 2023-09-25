120035ddb1ce8380e2ad630524bead9676c1404f
Amulya Garimella <amulya@dhcp-10-250-73-119.harvard.edu>
2023-09-25 10:16:16 -0400

    general function

--- get_segment_corr_stain.py
+++ get_segment_corr_stain.py
@@ -46,33 +46,85 @@ def get_cellpose_masks (array_img, diameter=90, cellprob_threshold=0.4):
     masks, flows, styles, diams = model.eval(array_img, do_3D=True, diameter=diameter, cellprob_threshold=cellprob_threshold, channels=[0,None])
     return masks
 
-def (array_img, masks)
-    cell_labels = np.unique(masks)
-    for c in cell_labels:
-        c_mask = np.where(np.array(masks)==c)
-        bounds_0 = min(c_mask[0]), max(c_mask[0])
-        bounds_1 = min(c_mask[1]), max(c_mask[1])
-        bounds_2 = min(c_mask[2]), max(c_mask[2])
-        bounds = bounds_0[0]:bounds_0[1],bounds_1[0]:bounds_1[1],bounds_2[0]:bounds_2[1]
+def cut_to_label_bounds (masks, label, array_img):
+    c_mask = np.where(np.array(masks)==label)
+    bounds_0 = min(c_mask[0]), max(c_mask[0])
+    bounds_1 = min(c_mask[1]), max(c_mask[1])
+    bounds_2 = min(c_mask[2]), max(c_mask[2])
 
-        cell_stack_mask = np.array(masks[:])
-        cell_stack_mask[np.where(masks!=c)] = 0
+    cell_stack_mask = np.array(masks[:])
+    cell_stack_mask[np.where(masks!=label)] = 0
+    
+    cell_stack_mask = cell_stack_mask[bounds_0[0]:bounds_0[1],bounds_1[0]:bounds_1[1],bounds_2[0]:bounds_2[1]]
+    cell_stack_c0 = array_img[0][bounds_0[0]:bounds_0[1],bounds_1[0]:bounds_1[1],bounds_2[0]:bounds_2[1]]
+    cell_stack_c1 = array_img[1][bounds_0[0]:bounds_0[1],bounds_1[0]:bounds_1[1],bounds_2[0]:bounds_2[1]]
+    return cell_stack_mask, cell_stack_c0, cell_stack_c1
+
+def correlate_by_frame (array_img, masks, fn, norm=False, flatten = False, mip=False, mip_axis = None, per_frame=True):
+    cell_labels = np.unique(masks)
+    print(cell_labels)
+    # per_frame_corrs = []
+    results = [] 
+    for c in cell_labels:
+        if c == 0:
+            continue
+        c_corrs = []
+        cell_stack_mask, cell_stack_c0, cell_stack_c1 = cut_to_label_bounds(masks, c, array_img)
+      
+        # if per cell and MIP
+        # TODO mask !
+        if not per_frame and (mip and mip_axis is not None):
+            print("MIP unsupported")
+            return
+            mip_c0 = np.max(cell_stack_c0, axis=mip_axis)
+            mip_c1 = np.max(cell_stack_c1, axis=mip_axis)
+            if norm: 
+                mip_c0 = mip_c0/np.linalg.norm(mip_c0)
+                mip_c1 = mip_c0/np.linalg.norm(mip_c1)
+            if flatten:
+                mip_c0 = np.ravel(mip_c0)
+                mip_c1 = np.ravel(mip_c1)
+            res = fn(mip_c0, mip_c1)
+            results.append(res)
+            continue
         
-        cell_stack_mask = cell_stack_mask[bounds]
-        cell_stack_c0 = array_img[0][bounds]
-        cell_stack_c1 = array_img[1][bounds]
-        #c0_whole = []
-        #c1_whole = []
+        # if per frame and/or not MIP
+        c0_allframes = []
+        c1_allframes = []
+        c_frames_res = []
         for i in range(np.shape(cell_stack_mask)[0]):
-            c0_slice = np.ravel(cell_stack_c0[i])
-            c1_slice = np.ravel(cell_stack_c1[i])
-            mask_slice = np.ravel(cell_stack_mask[i])
-            mask_pixels = np.where(mask_slice!=0)[0]
-            if len(mask_pixels) < 2:
+            # Note: we mutate c0_slice and c1_slice directly.
+            c0_slice = cell_stack_c0[i]
+            c1_slice = cell_stack_c1[i]
+            mask_slice = cell_stack_mask[i]
+            mask_pixels = np.where(mask_slice!=0)
+            if not all([x >= 2 for x in np.shape(mask_pixels)]):
                 continue
-            #c0_whole = c0_whole + list(c0_slice)
-            #c1_whole = c1_whole + list(c1_slice)
-            #r, p = scipy.stats.pearsonr(c0_slice[mask_pixels],c1_slice[mask_pixels]) 
-            #cell_corrs.append(r)
-        r, p = scipy.stats.pearsonr(c0_whole,c1_whole)
-        corrs.append(r)
+            c0_slice = c0_slice[mask_pixels]
+            c1_slice = c1_slice[mask_pixels]
+            
+            # todo consider hstacking
+            if not per_frame:
+                c0_allframes = c0_allframes + list(np.ravel(c0_slice))
+                c1_allframes = c1_allframes + list(np.ravel(c1_slice))
+                continue
+            if norm: 
+                c0_slice = c0_slice/np.linalg.norm(c0_slice)
+                c1_slice = c1_slice/np.linalg.norm(c1_slice)
+            if flatten:
+                c0_slice = np.ravel(c0_slice)
+                c1_slice = np.ravel(c1_slice)
+            
+            res = fn(c0_slice,c1_slice) 
+            c_frames_res.append(res)
+        
+        if per_frame:
+            results.append(c_frames_res)
+
+        if not per_frame and flatten:
+            if norm:
+                c0_allframes = c0_allframes/np.linalg.norm(c0_allframes)
+                c1_allframes = c1_allframes/np.linalg.norm(c1_allframes)
+            res = fn(c0_allframes,c1_allframes)
+            results.append(res)
+    return results
